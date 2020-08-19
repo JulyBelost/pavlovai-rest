c7e96fce8fdbe2938357d5f4dc160419cf0855ba
JulyBelost <julybelost@gmail.com>
2020-08-19 15:48:00 +0300

    Add database model for products table

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float
+from db import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    price = Column(Float)
