965fa992b41d9ce8e32f2d6896e72790635170dd
JulyBelost <julybelost@gmail.com>
2020-08-19 17:13:22 +0300

    Add constructor and sequence to table

--- models.py
+++ models.py
@@ -1,11 +1,15 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Sequence
 from db import Base
 
 
 class Product(Base):
     __tablename__ = "products"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, Sequence(), primary_key=True)
     name = Column(String)
     description = Column(String)
     price = Column(Float)
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
